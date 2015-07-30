6e736089c51df1e80b27c10d369cd9dc2ffb1410
Momozono <tanatana1089@gmail.com>
2015-07-30 09:32:42 +0900

    fix indantation

--- iterator.rb
+++ iterator.rb
@@ -1,11 +1,11 @@
 array = %w[hoge huga foobar]
 # 内部イテレータ Enumarator#eachの中身みたいなもの
 def for_each_method(array)
-	i = 0
-	while i < array.length
-		p array[i]
-		i += 1
-	end
+  i = 0
+  while i < array.length
+    p array[i]
+    i += 1
+  end
 end
 
 for_each_method(array)
@@ -13,37 +13,37 @@ for_each_method(array)
 a = [10, 20, 30]
 
 a.each do |a|
-	puts a	
+  puts a  
 end
 
 
 class Account
-	attr_accessor :name, :balance
+  attr_accessor :name, :balance
 
-	def initialize(name, balance)
-		@name = name
-		@balance = balance
-	end
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
 
-	def <=> (other)
-		balance <=> other.balance
-	end
+  def <=> (other)
+    balance <=> other.balance
+  end
 end
 
 class Portfolio
-	include Enumerable
+  include Enumerable
 
-	def initialize
-		@accounts = []
-	end
+  def initialize
+    @accounts = []
+  end
 
-	def each(&block)
-		@accounts.each(&block)
-	end
+  def each(&block)
+    @accounts.each(&block)
+  end
 
-	def add_account(account)
-		@accounts << account
-	end
+  def add_account(account)
+    @accounts << account
+  end
 end
 
 momozono = Account.new("momozono", 12345)
@@ -53,10 +53,10 @@ my_portfolio.any? { |account| account.balance > 2000}.tap{|a|p a}
 
 
 def subclasses_of(superclass)
-	subclass = []
-	ObjectSpace.each_object(Class) do |k|
-		next if !k.ancestors.include?(superclass) || superclass == k || k.to_s.include?('::') || subclasses.include?(k.to_s)
-		subclasses << k.to_s
-	end
-	subclasses
+  subclass = []
+  ObjectSpace.each_object(Class) do |k|
+    next if !k.ancestors.include?(superclass) || superclass == k || k.to_s.include?('::') || subclasses.include?(k.to_s)
+    subclasses << k.to_s
+  end
+  subclasses
 end
